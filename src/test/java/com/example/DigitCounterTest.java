7b589945ffab008d299c53c3da006f891a8d44a6
Bruno Borges <brborges@microsoft.com>
2021-12-22 13:49:44 -0800

    reduce duplicated code

--- src/test/java/com/example/DigitCounterTest.java
+++ src/test/java/com/example/DigitCounterTest.java
@@ -2,97 +2,59 @@ package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.function.Function;
+
 import org.junit.jupiter.api.Test;
 
 public class DigitCounterTest {
 
-    @Test
-    public void test_countDigitsByWhile() {
+    private void testCounter(Function<Long, Integer> counter) {
         long number = 9;
-        var digitCounter = new DigitCounter();
 
         for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByWhile(number));
+            assertEquals(digits, counter.apply(number));
             number = number * 10L;
         }
+    }
+
+    @Test
+    public void test_countDigitsByWhile() {
+        testCounter(new DigitCounter()::countDigitsByWhile);
     }
 
     @Test
     public void test_countDigitsByStringLength() {
-        long number = 9;
-        var digitCounter = new DigitCounter();
-
-        for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByStringLength(number));
-            number = number * 10L;
-        }
+        testCounter(new DigitCounter()::countDigitsByStringLength);
     }
 
     @Test
     public void test_countDigitsByPowersOfTwo() {
-        long number = 9;
-        var digitCounter = new DigitCounter();
-
-        for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByPowersOfTwo(number));
-            number = number * 10L;
-        }
-
+        testCounter(new DigitCounter()::countDigitsByPowersOfTwo);
     }
 
     @Test
     public void test_countDigitsByLogarithmic() {
-        long number = 9;
-        var digitCounter = new DigitCounter();
-
-        for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByLogarithmic(number));
-            number = number * 10L;
-        }
+        testCounter(new DigitCounter()::countDigitsByLogarithmic);
     }
 
     @Test
     public void test_countDigitsByFor() {
-        long number = 9;
-        var digitCounter = new DigitCounter();
-
-        for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByFor(number));
-            number = number * 10L;
-        }
+        testCounter(new DigitCounter()::countDigitsByFor);
     }
 
     @Test
     public void test_countDigitsByDoWhile() {
-        long number = 9;
-        var digitCounter = new DigitCounter();
-
-        for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByDoWhile(number));
-            number = number * 10L;
-        }
+        testCounter(new DigitCounter()::countDigitsByDoWhile);
     }
 
     @Test
     public void test_countDigitsByDivision() {
-        long number = 9;
-        var digitCounter = new DigitCounter();
-
-        for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByDivision(number));
-            number = number * 10L;
-        }
+        testCounter(new DigitCounter()::countDigitsByDivision);
     }
 
     @Test
     public void test_countDigitsByDivideAndConquer() {
-        long number = 9;
-        var digitCounter = new DigitCounter();
-
-        for (int digits = 1; digits <= DigitCounter.MAX_DIGIT_COUNT; digits++) {
-            assertEquals(digits, digitCounter.countDigitsByDivideAndConquer(number));
-            number = number * 10L;
-        }
+        testCounter(new DigitCounter()::countDigitsByDivideAndConquer);
     }
 
 }
